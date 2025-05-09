781d5416f2a4a6c90be68505c5afb00b19ab052c
longchung <106843769+longchung@users.noreply.github.com>
2025-05-08 23:26:05 -0700

    add alpha v1.3 with sader added
    added crt tv shader and player flash hurt shader

--- 2d-platform-game/scripts/object/spike.gd
+++ 2d-platform-game/scripts/object/spike.gd
@@ -1,15 +1,10 @@
 extends Area2D
 #this script is for spike
-
-#when collide with a player, if score is larger than 0 then decrease it by 10
-#and if it is then set it to 0
-#then teleport the player by updating the player's gobal position
-#to that of the marker we set in the level which is back to the start 
+#when collide with a player it will teleport the player 
+#by updating the player's gobal position to that of the
+#marker we set in the level which is back to the start 
 #of the level
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.global_position=$"..".global_position
-		if AllWorld.total_score > 0:
-			AllWorld.total_score -= 10
-		else:
-			AllWorld.total_score = 0
+		
--- 2d-platform-game/scripts/player/player_spawn.gd
+++ 2d-platform-game/scripts/player/player_spawn.gd
@@ -6,9 +6,7 @@ extends Node2D
 #add the player to the scene then call the set_player_position function 
 #to update play gobal position to the spawner gobal position
 func _ready() -> void:
-	GlobalPlayerManager.add_player_instance()
-	if GlobalPlayerManager.player.get_parent():
-		GlobalPlayerManager.player.get_parent().remove_child(GlobalPlayerManager.player)	
+	GlobalPlayerManager.add_player_instance()	
 	self.add_child(GlobalPlayerManager.player)
 	GlobalPlayerManager.set_player_position(global_position)
 
